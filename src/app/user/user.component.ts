9a0a02568dff930f82de6b03f51f3c5d1431b234
Ion Lefter <ionlefter1997@gmail.com>
2024-10-09 14:57:48 +0300

    update state using signal

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, Signal } from '@angular/core';
 
  import { DUMMY_USERS } from '../dummy_users';
 
@@ -11,14 +11,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  get imagePath(){
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  // get imagePath(){
+  //   return 'assets/users/' + this.selectedUser().avatar;
+  // }
+
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar) 
 
   onSelectUser(){
-    const randomIndex  = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser = DUMMY_USERS[randomIndex]
+    const randomIndex  = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
