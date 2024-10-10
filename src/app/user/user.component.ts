a000ed32384146efdd273d95d93665c56b3d5e70
Ion Lefter <ionlefter1997@gmail.com>
2024-10-10 10:06:42 +0300

    type aliases & interfaces

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,7 +2,17 @@ import { Component, computed, EventEmitter, Input, Output } from '@angular/core'
 
  import { DUMMY_USERS } from '../dummy_users';
 
- const randomIndex  = Math.floor(Math.random() * DUMMY_USERS.length)
+  // type User = {
+  //   id: string;
+  //   avatar: string;
+  //   name: string;
+  // }
+
+  interface User{
+    id: string;
+    avatar: string;
+    name: string;
+  }
 
 @Component({
   selector: 'app-user',
@@ -12,24 +22,12 @@ import { Component, computed, EventEmitter, Input, Output } from '@angular/core'
 })
 export class UserComponent {
 
-  // @Input({required: true}) id!: string;
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-  @Input({required: true}) user!:{
-    id: string;
-    avatar: string;
-    name: string;
-  };
+  @Input({required: true}) user!: User;
   @Output() select = new EventEmitter<string>();
 
   imagePath = computed(() => {
     return 'assets/users/' + this.user.avatar;
   });
-
-  // get imagePath(){
-  //   return 'assets/users/' + this.avatar;
-  // }
-
 
   onSelectUser(){
       this.select.emit(this.user.id);
